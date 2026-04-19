79fa43dfdcceff3a8f5126f2e8732daa929908fe
sergei-aero <pavlov.aero@gmail.com>
2026-04-19 15:28:11 +0300

    Замена FBV на CBV для home, product_detail, contactsстатики

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import ProductListView, ProductDetailView, ContactsView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', ProductListView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Product
 
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'catalog/home.html', {'products': products})
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
-def product_detail(request, pk):
-    from django.shortcuts import get_object_or_404
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
