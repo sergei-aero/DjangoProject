89d14515b2b757d35edfb59ee765a298f434c66b
sergei-aero <pavlov.aero@gmail.com>
2026-05-20 12:42:32 +0400

    Добавлено кэширование с Redis, страница категорий, низкоуровневое кэширование

--- config/settings.py
+++ config/settings.py
@@ -128,4 +128,14 @@ DEFAULT_FROM_EMAIL = 'webmaster@localhost'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'catalog:product_list'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 
